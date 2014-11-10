85548b8d36d1b201edaf3d520c21b78dde283360
bttf <adnanchowdhury88@gmail.com>
2014-11-09 22:14:40 -0500

    reworked to use blessed library; a real mess though

--- app.js
+++ app.js
@@ -1,34 +1,58 @@
-var Canvas = require('drawille');
 var line = require('bresenham');
-
-var buffer = 0;
-var w = 320,
-    h = 112,
-    x = (w / 2),
-    y = (h / 2),
-    z = (w + h) / 2;
-var tick = 0;
-var c =  new Canvas(w, h);
-var ratio = 128;
-var speed = 0.5;
-var newSpeed = 18;
-var n = 256;
+var canvas = require('drawille');
+var blessed = require('blessed');
+var screen = blessed.screen();
+var shipName = 'U.S.S. Chowdhury';
+var viewScreenLabel = 'ViewScreen';
 var stars = [];
+var graph;
+var c;
+var n, speed, ratio, w, h, x, y, z;
 
-function init() {
-  for (var i = 0; i < n; i++) {
-    stars[i] = new Array(7);
-    stars[i][0] = Math.random() * w * 2 - x * 2;
-    stars[i][1] = Math.random() * h * 2 - y * 2;
-    stars[i][2] = Math.round(Math.random() * z);
-    stars[i][3] = 0;
-    stars[i][4] = 0;
-    stars[i][5] = 0;
-    stars[i][6] = 0;
-  }
-}
 
-function render() {
+var drawHeader = function() {
+  var headerText = shipName;
+
+  var header = blessed.text({
+    top: 'top',
+    left: 'left',
+    width: headerText.length,
+    height: '1',
+    fg: {
+      "fg": "#a537fd"	
+    },
+    content: headerText,
+    tags: true
+  });
+
+  var date = blessed.text({
+    top: 'top',
+    right: 0,
+    width: 9,
+    height: '1',
+    align: 'right',
+    content: '',
+    tags: true
+  });
+
+  screen.append(header);
+  screen.append(date);
+
+  var zeroPad = function(input) {
+    return ('0' + input).slice(-2);
+  };
+
+  var updateTime = function() {
+    var time = new Date();
+    date.setContent(zeroPad(time.getHours()) + ':' + zeroPad(time.getMinutes()) + ':' + zeroPad(time.getSeconds()) + ' ');
+    screen.render();
+  };
+
+  updateTime();
+  setInterval(updateTime, 1000);
+};
+
+var drawStars = function() {
   for (var i = 0; i < n; i++) {
     stars[i][5] = stars[i][3];
     stars[i][6] = stars[i][4];
@@ -45,28 +69,74 @@ function render() {
     stars[i][3] = x + (stars[i][0] / stars[i][2]) * ratio;
     stars[i][4] = y + (stars[i][1] / stars[i][2]) * ratio;
   }
-  if (speed < newSpeed) {
-    speed += 0.01;
-  }
-}
-
-function draw() {
   c.clear();
 
   for (var i = 0; i < n; i++) {
     if (stars[i][5] > 0 && stars[i][5] < w && stars[i][6] > 0 && stars[i][6] < h) {
-        line(stars[i][5], stars[i][6], stars[i][3], stars[i][4], c.set.bind(c));
+      line(stars[i][5], stars[i][6], stars[i][3], stars[i][4], c.set.bind(c));
     }
   }
-  process.stdout.write(c.frame());
-  process.stdout.write('debug: speed ' + speed + '');
-}
+  var txtOutput = c.frame().split("\n");
+  txtOutput[0] = 'w: ' + w + ', h: ' + h;
+  // txtOutput[1] = h;
+  // txtOutput[2] = graph.width;
+  // txtOutput[3] = graph.height;
+  // txtOutput[4] = stars.length;
+  // txtOutput[5] = stars[1][0] + ' ' +
+  //                stars[1][1] + ' ' +
+  //                stars[1][2] + ' ' +
+  //                stars[1][3] + ' ' +
+  //                '\n' +
+  //                stars[1][4] + ' ' +
+  //                stars[1][5] + ' ' +
+  //                stars[1][6];
+  graph.setContent(txtOutput.join("\n"));
+  screen.render();
+};
 
-function loop() {
-  render();
-  draw();
-}
+var init = function() {
+  drawHeader();
+
+  graph = blessed.box({
+    top: 1,
+    left: 'left',
+    width: '100%',
+    height: '98%',
+    content: '',
+    fg: "#a537fd",
+    tags: true,
+    border: {
+      "type": "line",
+      "fg": "#00ebbe"
+    },
+    label: ' ' + viewScreenLabel + ' '
+  });
+
+  screen.append(graph);
+  n = 128;
+  speed = 5;
+  w = (graph.width - (graph.width % 4)) * 2;
+  h = (graph.height - (graph.height % 4)) * 2;
+  ratio = w / 2;
+  x = (w / 2);
+  y = (h / 2);
+  z = (w + h) / 2;
+  c = new canvas(w, h);
+
+  for (var i = 0; i < n; i++) {
+    stars[i] = new Array(7);
+    stars[i][0] = Math.random() * w * 2 - x * 2;
+    stars[i][1] = Math.random() * h * 2 - y * 2;
+    stars[i][2] = Math.round(Math.random() * z);
+    stars[i][3] = 0;
+    stars[i][4] = 0;
+    stars[i][5] = 0;
+    stars[i][6] = 0;
+  }
+
+  drawStars();
+  setInterval(drawStars, 1000/60);
+  screen.render()
+};
 
 init();
-setInterval(loop, 1000/60);
-
